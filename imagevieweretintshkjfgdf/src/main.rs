39bde3ff535491c9266cdb2cf1b77602de0bf868
PogtatoDev <pogtatodev@gmail.com>
2026-08-22 04:39:03 +0100

    extra stuff

--- imagevieweretintshkjfgdf/src/main.rs
+++ imagevieweretintshkjfgdf/src/main.rs
@@ -1,12 +1,11 @@
 use minifb::*;
-use rand::*;
 
 const HEIGHT: usize = 1024;
 const WIDTH: usize = 1024;
 
 struct Vector2<T> {
     x: T,
-    y: T
+    y: T,
 }
 
 impl<T> Vector2<T> {
@@ -22,7 +21,7 @@ fn index_to_position(index: u32) -> Vector2<u32> {
     )
 }
 
-fn position_to_index(position: Vector2<u32>) -> u32 {
+fn position_to_index(position: &Vector2<u32>) -> u32 {
     (position.y as usize * WIDTH + position.x as usize) as u32
 }
 
@@ -31,18 +30,69 @@ fn rgb(r: u32, g: u32, b: u32) -> u32 {
     (r << 16) | (g << 8) | b
 }
 
+fn draw_pixel(position: &Vector2<u32>, color: u32, buffer: &mut Vec<u32>) -> () {
+    let idx: u32 = position_to_index(position);
+    buffer[idx as usize] = color;
+}
+
+fn draw_rect(
+    position: Vector2<u32>,
+    color: u32,
+    width: u32,
+    height: u32,
+    filled: bool,
+    mut buffer: &mut Vec<u32>,
+) -> () {
+    let mut cursor: Vector2<u32> = Vector2::new(position.x, position.y);
+    if filled {
+        let mut counter: u32 = 0;
+        for i in 0..(width * height) {
+            if i % width == 0 {
+                counter += 1;
+                cursor = Vector2::new(position.x, position.y + counter);
+            }
+            cursor.x += 1;
+            draw_pixel(&cursor, color, &mut buffer);
+        }
+    } else {
+       for _ in 0..width {
+           cursor.x += 1;
+           draw_pixel(&cursor, color, &mut buffer);
+       }
+       for _ in 0..height {
+           cursor.y += 1;
+           draw_pixel(&cursor, color, &mut buffer);
+       }
+       for _ in 0..width {
+           cursor.x -= 1;
+           draw_pixel(&cursor, color, &mut buffer);
+       }
+       for _ in 0..height {
+           cursor.y -= 1;
+           draw_pixel(&cursor, color, &mut buffer);
+       }
+    }
+}
 
 fn main() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
     let mut window: Window = Window::new("i3 floating", WIDTH, HEIGHT, WindowOptions::default())
         .unwrap_or_else(|e| panic!("{}", e));
-    let mut rng = rand::rng();
-    window.set_target_fps(0);
+    window.set_target_fps(30);
+    let mut temp: bool = false;
 
-    while window.is_open() {
-        for (i, pixel) in buffer.iter_mut().enumerate() {
-
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for pixel in buffer.iter_mut() {
+            *pixel = 0;
         }
+        draw_rect(
+            Vector2::new(50, 50),
+            rgb(255, 0, 0),
+            100,
+            100,
+            true,
+            &mut buffer,
+        );
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
 }
